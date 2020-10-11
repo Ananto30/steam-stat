2e25aae6a9ad01426b5363b505e53e15138457d2
Ananto30 <azizulhaq.ananto@gmail.com>
2020-10-11 19:14:42 +0600

    Add error handling

--- api/index.js
+++ api/index.js
@@ -11,34 +11,42 @@ const steam = new SteamAPI(process.env.STEAM_APP_ID);
 module.exports = async (req, res) => {
   const { profileName, profileUrl } = req.query;
 
-  let steamId;
-  if (profileName) {
-    steamId = await steam.resolve(
-      "https://steamcommunity.com/id/" + profileName
+  try {
+    let steamId;
+    if (profileName) {
+      steamId = await steam.resolve(
+        "https://steamcommunity.com/id/" + profileName
+      );
+    } else if (profileUrl) {
+      steamId = await steam.resolve(profileUrl);
+    } else {
+      return res.status(400).send("Put url or profileName");
+    }
+
+    const summary = await steam.getUserSummary(steamId);
+
+    let recentGames = await steam.getUserRecentGames(steamId);
+    await downloadRecentGamesImages(recentGames);
+
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.setHeader("Cache-Control", `public, max-age=10800`);
+    return res.send(
+      renderRecentStatCard(
+        {
+          nickname: summary.nickname,
+          steamProfileUrl: summary.url,
+          avatarMedium: await imageToData(summary.avatar.medium),
+          recentPlayHours: countRecentPlayHours(recentGames),
+          personaState: convertPersonaState(summary.personaState),
+        },
+        recentGames
+      )
     );
-  } else if (profileUrl) {
-    steamId = await steam.resolve(profileUrl);
-  } else {
-    return res.status(500).send("Put url or profileName");
+  } catch (e) {
+    if (e instanceof TypeError) {
+      return res.status(404).send("Profile not found");
+    }
+    console.trace(e);
+    return res.status(500).send("Something is wrong!");
   }
-
-  const summary = await steam.getUserSummary(steamId);
-
-  let recentGames = await steam.getUserRecentGames(steamId);
-  await downloadRecentGamesImages(recentGames);
-
-  res.setHeader("Content-Type", "image/svg+xml");
-  res.setHeader("Cache-Control", `public, max-age=10800`);
-  return res.send(
-    renderRecentStatCard(
-      {
-        nickname: summary.nickname,
-        steamProfileUrl: summary.url,
-        avatarMedium: await imageToData(summary.avatar.medium),
-        recentPlayHours: countRecentPlayHours(recentGames),
-        personaState: convertPersonaState(summary.personaState),
-      },
-      recentGames
-    )
-  );
 };
